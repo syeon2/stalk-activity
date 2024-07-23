a307ca90af9bfde20a2756f6b90d1b7b05588c30
SUYEON KIM <waterkite94@gmail.com>
2024-07-23 13:37:55 +0900

    Init Stalk Activity Service

---
+++ src/main/kotlin/io/waterkite94/stalk/activityservice/ActivityServiceApplication.kt
@@ -0,0 +1,11 @@
+package io.waterkite94.stalk.activityservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActivityServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ActivityServiceApplication>(*args)
+}
---
+++ src/test/kotlin/io/waterkite94/stalk/activityservice/ActivityServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.waterkite94.stalk.activityservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActivityServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
