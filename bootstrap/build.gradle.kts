2310e40115dda0b11fcba565aa01384d015175c9
SUYEON KIM <waterkite94@gmail.com>
2024-07-23 14:10:49 +0900

    [Feat] set eureka, cloud config, amqp

--- bootstrap/build.gradle.kts
+++ bootstrap/build.gradle.kts
@@ -1,4 +1,7 @@
 dependencies {
+    implementation(project(":support:msa-core"))
+    implementation(project(":support:monitor"))
+
     implementation("org.springframework.boot:spring-boot-starter-web")
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,9 @@
 rootProject.name = "activity-service"
 
 include(
-    "bootstrap"
+    "bootstrap",
+    "support:msa-core",
+    "support:monitor"
 )
 
 pluginManagement {
