9fb1f1e876f574992b8713c36850829be7e16e8d
SUYEON KIM <waterkite94@gmail.com>
2024-08-01 00:19:56 +0900

    [Fix] add transaction annotation repository funs

--- infrastructure/persistence/src/main/java/io/waterkite94/stalk/persistence/repository/PostLikeRepository.java
+++ infrastructure/persistence/src/main/java/io/waterkite94/stalk/persistence/repository/PostLikeRepository.java
@@ -1,10 +1,14 @@
 package io.waterkite94.stalk.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import io.waterkite94.stalk.persistence.enrity.PostLikeEntity;
 
 public interface PostLikeRepository extends JpaRepository<PostLikeEntity, Long> {
 
+	@Modifying(clearAutomatically = true)
+	@Transactional
 	void deleteByMemberIdAndPostId(String memberId, String postId);
 }
